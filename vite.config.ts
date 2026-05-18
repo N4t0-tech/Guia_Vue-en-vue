1a0927da4bd40a35c359d40b0a5c97996e5e7377
Renato Campos <renaxd13@gmail.com>
2026-05-18 14:32:35 -0400

    deploy en github

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Guia_Vue-en-vue/',
   plugins: [
     vue(),
     vueDevTools(),
